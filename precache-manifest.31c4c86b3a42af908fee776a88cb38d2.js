4c634bce47ad8248a9f0ffad63aeddbfb7d0f8f1
Saikrishnan <krishnansai99@gmail.com>
2020-08-09 17:10:58 +0530

    Updates

---
+++ precache-manifest.31c4c86b3a42af908fee776a88cb38d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e68b6d46620f608b5db50f18ef7b99",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "222a837c06e0ac436bf6",
+    "url": "/mywebsite/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6036fad361ce379afb56",
+    "url": "/mywebsite/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "222a837c06e0ac436bf6",
+    "url": "/mywebsite/static/js/2.78765c0d.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/mywebsite/static/js/2.78765c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6036fad361ce379afb56",
+    "url": "/mywebsite/static/js/main.d440b38e.chunk.js"
+  },
+  {
+    "revision": "275c5475de88963006f8",
+    "url": "/mywebsite/static/js/runtime-main.a28ebe82.js"
+  },
+  {
+    "revision": "c73d8d0b48053254d90385a03cf14e65",
+    "url": "/mywebsite/static/media/resume.c73d8d0b.pdf"
+  }
+]);
