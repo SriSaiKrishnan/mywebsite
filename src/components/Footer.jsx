6fdfdc42a9b5c22fd089f265e5ba6e5f3db81921
Saikrishnan <krishnansai99@gmail.com>
2020-08-09 16:32:39 +0530

    Create a React app and publish it to GitHub Pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,10 +10,10 @@ const Footer = () => {
       <a
         className="badge badge-dark"
         rel="noopener"
-        href="https://github.com/hashirshoaeb"
+        href="https://github.com/SriSaiKrishnan"
         aria-label="My GitHub"
       >
-        Hashir Shoaib
+        Sri Sai Krishnan
       </a>{" "}
       using <i className="fab fa-react"></i>
       <p>
